b5830a2e37997839bf5c5d62b826439535f7a389
Sxturn7 <satvikbhukya07@gmail.com>
2026-06-24 12:49:32 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,15 +3,7 @@ import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 //import './index.css';
 createRoot(document.getElementById('root')!).render(
-  <div style={{
-    background: 'black',
-    color: 'red',
-    fontSize: '50px',
-    height: '100vh',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center'
-  }}>
-    ROOT WORKING
-  </div>
+  <StrictMode>
+    <App />
+  </StrictMode>
 );
