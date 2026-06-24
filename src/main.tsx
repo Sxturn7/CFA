921ed6ec2bffb60bd1b68078266796bcb4c5a445
Sxturn7 <satvikbhukya07@gmail.com>
2026-06-24 12:47:01 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,17 @@ import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 //import './index.css';
 
-createRoot(document.getElementById('root')).render(
+
+createRoot(document.getElementById('root')!).render(
   <div style={{
     background: 'black',
     color: 'red',
     fontSize: '50px',
-    height: '100vh'
+    height: '100vh',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center'
   }}>
     ROOT WORKING
   </div>
+);
